8b891bc8a6b8ffda30d74f05c77b3644db9cb615
Raul Santiago Insuasty <r.insuasty@uniandes.edu.co>
2025-07-19 19:56:01 -0500

    cambios a gemini flash para mejorar la velocidad

--- api/redactarManualGemini.js
+++ api/redactarManualGemini.js
@@ -32,7 +32,7 @@ Redacta el texto corregido a continuación, sin encabezado ni comentarios adicio
 `;
 
   try {
-    const model = geminiClient.getGenerativeModel({ model: "gemini-2.5-pro" });
+    const model = geminiClient.getGenerativeModel({ model: "gemini-2.5-flash" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const texto_mejorado = response.text();
